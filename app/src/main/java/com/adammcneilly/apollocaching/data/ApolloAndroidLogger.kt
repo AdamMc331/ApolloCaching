fa3398210cf22a55e45528a72cb2f5bd63af01c7
amcneilly <amcneilly331@gmail.com>
2020-12-29 12:27:30 -0500

    Replacing logging interceptor with Apollo approach.

---
+++ app/src/main/java/com/adammcneilly/apollocaching/data/ApolloAndroidLogger.kt
@@ -0,0 +1,22 @@
+package com.adammcneilly.apollocaching.data
+
+import android.util.Log
+import com.apollographql.apollo.Logger
+
+/**
+ * This is a custom implementation of a [Logger] from Apollo that will take any
+ * messages and log them in the Android logcat for debugging purposes.
+ */
+class ApolloAndroidLogger : Logger {
+    override fun log(priority: Int, message: String, t: Throwable?, vararg args: Any) {
+        val formattedMessage = message.format(*args)
+        val tag = ApolloAndroidLogger::class.java.simpleName
+
+        when (priority) {
+            Logger.DEBUG -> Log.d(tag, formattedMessage)
+            Logger.WARN -> Log.w(tag, formattedMessage)
+            Logger.ERROR -> Log.e(tag, formattedMessage)
+            else -> Log.d(tag, formattedMessage)
+        }
+    }
+}
--- app/src/main/java/com/adammcneilly/apollocaching/di/NetworkingModule.kt
+++ app/src/main/java/com/adammcneilly/apollocaching/di/NetworkingModule.kt
@@ -1,11 +1,10 @@
 package com.adammcneilly.apollocaching.di
 
-import com.adammcneilly.apollocaching.data.HttpCacheLoggingInterceptor
+import com.adammcneilly.apollocaching.data.ApolloAndroidLogger
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.cache.http.HttpCache
 import com.apollographql.apollo.cache.http.ApolloHttpCache
 import com.apollographql.apollo.cache.http.DiskLruHttpCacheStore
-import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import java.io.File
@@ -15,25 +14,29 @@ private const val KILOBYTES_PER_MEGABYTE = 1024L
 
 val networkingModule = module {
 
-    factory {
-        OkHttpClient.Builder()
-            .addInterceptor(HttpCacheLoggingInterceptor())
-            .build()
-    }
-
+    /**
+     * An [HttpCache] is a limited but easier to use caching layer for the Apollo library.
+     *
+     * We create the [File] that our cache is stored in, define the size of our cache in bytes,
+     * and return this instance.
+     *
+     * Full docs: https://www.apollographql.com/docs/android/essentials/http-cache/
+     */
     factory<HttpCache> {
         val file = File(androidContext().cacheDir, "apolloCache")
         val numMegabytes = 1
         val sizeInMegabytes = BYTES_PER_KILOBYTE * KILOBYTES_PER_MEGABYTE * numMegabytes
 
-        ApolloHttpCache(DiskLruHttpCacheStore(file, sizeInMegabytes))
+        val cacheStore = DiskLruHttpCacheStore(file, sizeInMegabytes)
+        val logger = ApolloAndroidLogger()
+
+        ApolloHttpCache(cacheStore, logger)
     }
 
     factory {
         ApolloClient.builder()
             .serverUrl("https://countries.trevorblades.com/")
             .httpCache(get())
-            .okHttpClient(get())
             .build()
     }
 }
