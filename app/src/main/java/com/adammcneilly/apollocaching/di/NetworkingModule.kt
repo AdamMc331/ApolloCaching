277a9626d9384b04ea14f4615d0d300ff1880268
Adam McNeilly <amcneilly331@gmail.com>
2020-12-29 23:15:04 -0500

    Adding SQL normalized cache.  (#4)
    * Chaining with sqlite cache.

--- app/src/main/java/com/adammcneilly/apollocaching/di/NetworkingModule.kt
+++ app/src/main/java/com/adammcneilly/apollocaching/di/NetworkingModule.kt
@@ -4,11 +4,14 @@ import com.adammcneilly.apollocaching.data.ApolloAndroidLogger
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.Logger
 import com.apollographql.apollo.api.cache.http.HttpCache
+import com.apollographql.apollo.api.cache.http.HttpCachePolicy
 import com.apollographql.apollo.cache.http.ApolloHttpCache
 import com.apollographql.apollo.cache.http.DiskLruHttpCacheStore
 import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory
 import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy
 import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory
+import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory
+import com.apollographql.apollo.fetcher.ApolloResponseFetchers
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import java.io.File
@@ -40,7 +43,7 @@ val networkingModule = module {
         ApolloHttpCache(cacheStore, get())
     }
 
-    single<NormalizedCacheFactory<*>> {
+    single {
         val numMegabytes = 10
         val sizeInMegabytes = BYTES_PER_KILOBYTE * KILOBYTES_PER_MEGABYTE * numMegabytes
 
@@ -52,11 +55,24 @@ val networkingModule = module {
     }
 
     single {
+        SqlNormalizedCacheFactory(androidContext(), "apollo.db")
+    }
+
+    single<NormalizedCacheFactory<*>> {
+        val inMemoryCache = get<LruNormalizedCacheFactory>()
+        val sqliteCache = get<SqlNormalizedCacheFactory>()
+        val memoryThenSqliteCache = inMemoryCache.chain(sqliteCache)
+        memoryThenSqliteCache
+    }
+
+    single {
         ApolloClient.builder()
             .serverUrl("https://countries.trevorblades.com/")
             .httpCache(get())
             .normalizedCache(get())
             .logger(get())
+            .defaultHttpCachePolicy(HttpCachePolicy.CACHE_FIRST)
+            .defaultResponseFetcher(ApolloResponseFetchers.CACHE_FIRST)
             .build()
     }
 }
