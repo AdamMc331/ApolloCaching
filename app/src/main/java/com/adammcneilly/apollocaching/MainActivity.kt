287b1de48a37ae63907c58c59f0606e36523ce6e
Adam McNeilly <amcneilly331@gmail.com>
2021-01-15 21:30:00 -0500

    Adding helper methods to print caches to logcat.  (#5)
    * Printing out what's in the HttpCache.
    
    * Printing normalized cache.

--- app/src/main/java/com/adammcneilly/apollocaching/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/apollocaching/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.adammcneilly.apollocaching
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.adammcneilly.apollocaching.databinding.ActivityMainBinding
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.NormalizedCache
+import org.koin.android.ext.android.get
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +22,36 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         setupNavigation()
+        printCaches()
     }
 
     private fun setupNavigation() {
         val navController = findNavController(R.id.nav_host_fragment)
         binding.toolbar.setupWithNavController(navController)
+    }
+
+    /**
+     * This is a utility method that prints information in the Logcat of what is in our Http and
+     * normalized caches when the activity starts up.
+     */
+    private fun printCaches() {
+        printHttpCache()
+        printNormalizedCache()
+    }
+
+    private fun printNormalizedCache() {
+        val apolloClient: ApolloClient = get()
+        val normalizedCacheDump = apolloClient.apolloStore.normalizedCache().dump()
+        val formattedDump = NormalizedCache.prettifyDump(normalizedCacheDump)
+        Log.d("ApolloNormalizedCache", formattedDump)
+    }
+
+    private fun printHttpCache() {
+        val cacheDirectory = File(applicationContext.cacheDir, "apolloCache")
+        cacheDirectory.listFiles()?.forEach { file ->
+            Log.d("ApolloHttpCache", "HttpCacheFile: ${file.name}")
+            Log.d("ApolloHttpCache", "HttpCacheFile: ${file.readText()}")
+            Log.d("ApolloHttpCache", "-----")
+        }
     }
 }
