a85691899320db8d48835fd5ebf3c3a4b4fabec9
adammc331 <amcneilly331@gmail.com>
2021-01-15 21:20:19 -0500

    Printing out what's in the HttpCache.

--- app/src/main/java/com/adammcneilly/apollocaching/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/apollocaching/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.adammcneilly.apollocaching
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.adammcneilly.apollocaching.databinding.ActivityMainBinding
+import org.koin.android.ext.koin.androidContext
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +20,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         setupNavigation()
+        printCaches()
     }
 
     private fun setupNavigation() {
         val navController = findNavController(R.id.nav_host_fragment)
         binding.toolbar.setupWithNavController(navController)
+    }
+
+    /**
+     * This is a utility method that prints information in the Logcat of what is in our Http and
+     * normalized caches when the activity starts up.
+     */
+    private fun printCaches() {
+        printHttpCache()
+    }
+
+    private fun printHttpCache() {
+        val cacheDirectory = File(applicationContext.cacheDir, "apolloCache")
+        cacheDirectory.listFiles()?.forEach { file ->
+            Log.d("ApolloHttpCache", "HttpCacheFile: ${file.name}")
+            Log.d("ApolloHttpCache", "HttpCacheFile: ${file.readText()}")
+            Log.d("ApolloHttpCache", "-----")
+        }
     }
 }
